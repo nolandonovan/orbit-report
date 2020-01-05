30e4123d5370fd913770f2095ff742bba64becc7
nolandonovan <40967397+nolandonovan@users.noreply.github.com>
2020-01-05 00:14:50 -0600

    complete
    completed assignment

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,4 +12,12 @@ export class Satellite {
         this.orbitType = orbitType, 
         this.operational = operational
     }
+
+    shouldShowWarning(){
+        if(this.type == 'Space Debris'){
+            return true
+        } else {
+            return false
+        }
+    }
 }
